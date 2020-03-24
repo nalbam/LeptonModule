28fcf81bd148a1862f5ffde9c7e00e2374c53342
nalbam <me@nalbam.com>
2020-03-24 19:04:55 +0900

    crosshair max pos

--- software/raspberrypi_video/LeptonThread.cpp
+++ software/raspberrypi_video/LeptonThread.cpp
@@ -347,11 +347,11 @@ void LeptonThread::run()
 		//crosshair
 		for (int i = -1; i < 2; i++)
 		{
-			if (max_column + i >= 0)
+			if (max_column + i >= 0 && max_column < myImageWidth)
 			{
 				myImage.setPixel((max_column + i), max_row, crosshair);
 			}
-			if (max_row + i >= 0)
+			if (max_row + i >= 0 && max_row < myImageHeight)
 			{
 				myImage.setPixel(max_column, (max_row + i), crosshair);
 			}
